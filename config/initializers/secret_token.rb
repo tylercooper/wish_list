63a33da753d5ec5757406a806e85cad7f8862f86
tyler.cooper <tyler.cooper@ip-10-112-87-205.ec2.internal>
2011-04-21 00:01:37 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WishApp::Application.config.secret_token = 'f996d3d094f97121fe6c5ea3dca4f6eb1b029eae72ab5cb243afdc0d2eefb492dc9c40647b7b36e7dcc810a50ca7bb65d2774b9ba5a7ab3f465aaaf963ecfd18'
